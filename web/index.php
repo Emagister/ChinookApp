73bbef662ad4cfcbb07c4f7229b0e6f2ad0f002b
Christian <csoronellas@emagister.com>
2012-03-16 00:30:46 +0100

    Updated the index.php file

--- web/index.php
+++ web/index.php
@@ -4,13 +4,12 @@ require __DIR__ . '/silex.phar';
 require dirname(__DIR__) . '/vendor/.composer/autoload.php';
 require dirname(__DIR__) . '/app/Bootstrap.php';
 
-$bootstrap = new Bootstrap();
-$app = $bootstrap->bootstrap(new Silex\Application());
-
 /**
  * To run the application with the reverse proxy cache
  * enable the provider in the providers config file and
  * replace the application run with this
  */
 // $app['http_cache']->run();
-$app->run();
+$bootstrap = new Bootstrap();
+$bootstrap->bootstrap(new Silex\Application())
+          ->run();
